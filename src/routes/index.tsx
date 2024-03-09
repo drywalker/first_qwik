9c05ac630007caf4fdb2d7754355e4ea125024d1
unknown <slw_nbm@163.com>
2024-03-09 19:46:59 +0800

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Hi 👋</h1>
+
+      <p>这是Shawn·Lee·Walker的网站</p>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
